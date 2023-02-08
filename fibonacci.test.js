63c6838341c97839355650a46fa1b78fb2b90c1e
Will Adamson <william.adamson@roq.co.uk>
2023-02-08 11:30:48 +0000

    Fibonacci Tests 1.0

--- fibonacci.test.js
+++ fibonacci.test.js
@@ -10,3 +10,48 @@ test('When initialized the next number can be returned', () => {
     fibonacci.init(8);
     expect(fibonacci.next()).toBe(13);
 });
+
+test('Using the next function from new', () => {
+    //let nextValue = fibonacci.next();
+    //expect(nextValue).toBe(1)
+    expect(fibonacci.next()).toBe(1);
+});
+
+test('When initialized skip can be used', () => {
+    fibonacci.init(2);
+    expect(fibonacci.skip(4)).toBe(13);
+});
+
+test('Initialize from 0 should throw error', () => {
+    expect(() => {
+        fibonacci.init(0);
+    }).toThrow();
+});
+
+test('Initialize from a non-Fib should throw error', () => {
+    expect(() => {
+        fibonacci.init(7);
+    }).toThrow();
+});
+
+test('Skipping backwards should throw error', () => {
+    fibonacci.init(8);
+    expect(() => {
+        fibonacci.skip(-3);
+    }).toThrow();
+});
+// Would expect it to be 2 if skipping back was possible ; test failed so it's a DEFECT
+
+test('New test - next next next', () => {
+    fibonacci.next();
+    fibonacci.next();
+    fibonacci.next();
+    fibonacci.next();
+    fibonacci.next();
+    expect(fibonacci.next()).toBe(13);
+});
+
+test('When initialised, skip will jump to specified XL number', () => {
+    fibonacci.init(8);
+    expect(fibonacci.skip(20)).toBe(121393);
+})
